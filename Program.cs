7f97a3228016b96c660734b888006c07ca786080
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-08-29 16:54:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Q2_Array_class;
+
+namespace Q2_Array_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] stu = new Student[2];
+            for (int i = 0; i < stu.Length; i++)
+            {
+                stu[i] = new Student();
+                Console.WriteLine("Enter rollno");
+                stu[i].rollno= Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter sname");
+                stu[i].StuName=Console.ReadLine();
+                Console.WriteLine("Enter Marks");
+                stu[i].marks=Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter City");
+                stu[i].City = Console.ReadLine();
+
+            }
+            foreach (var item in stu)
+            {
+                Console.WriteLine("Following are the details");
+                Console.WriteLine(item.rollno);
+                Console.WriteLine(item.StuName);
+                Console.WriteLine(item.marks);
+                Console.WriteLine(item.City);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Studentclass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Q2_Array_class
+{
+    public class Student
+    {
+        private int _rollno;
+    
+        public int rollno
+        {
+            get { return _rollno; }
+            set { _rollno = value; }
+        }
+
+        private string _sname;
+
+        public string StuName
+        {
+            get { return _sname; }
+            set { _sname = value; }
+        }
+
+        private int _marks;
+
+        public int marks
+        {
+            get { return _marks; }
+            set { _marks = value; }
+        }
+
+            public string City { get; set; }    
+        }
+    }
+
