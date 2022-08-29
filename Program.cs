2f28d1b5d1577658541cbd067045e1a168f2e931
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-08-29 16:59:09 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q4_strings
+{
+    internal class Program
+    {
+       public static void Main(string[] args)
+        {
+            var names = new LinkedList<string>();
+            names.AddFirst("Deepak");
+            names.AddLast("Sam");
+            names.AddAfter(names.First, "Ram");
+            names.AddBefore(names.Last, "Avi");
+            names.AddBefore(names.Last.Previous, "Kajal");
+            names.AddAfter(names.First.Next, "Vaibhav");
+            //LinkedListNode<String> node = names.Find("Peter");
+            //names.AddBefore(node, "John");
+            //names.AddAfter(node, "Lucy");
+
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Q4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3_Product
+{
+    internal class Q4
+    {
+        public static void Main(string[] args)
+        {
+            var names = new LinkedList<string>();
+            names.AddLast("Sonoo Jaiswal");
+            names.AddLast("Ankit");
+            names.AddLast("Peter");
+            names.AddLast("Irfan");
+
+            foreach (var item in names)
+            {
+                Console.WriteLine(names);
+            }
+            Console.ReadLine();
+        }
+    }
+}
